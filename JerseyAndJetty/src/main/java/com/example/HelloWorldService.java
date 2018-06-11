0e2835fd530be11079dfc2a9ec8c3e25b65cf361
mahdieh68 <mahdieh.najafy@gmail.com>
2018-06-11 16:20:47 +0430

    jersey on jettty

---
+++ JerseyAndJetty/src/main/java/com/example/HelloWorldService.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+/**
+ * Created by najafi_takam on 04/15/2018.
+ */
+@Path("/hello")
+public class HelloWorldService {
+
+    @GET
+    @Path("/{param}")
+    public Response getMsg(@PathParam("param") String msg) {
+
+        String output = "Jersey say : " + msg;
+
+        return Response.status(200).entity(output).build();
+
+    }
+
+}
